607fccf315184c9d70a384913092601606f1d4ed
Hanna Putiprawan <hputiprawan2@gmail.com>
2020-12-29 01:03:12 -0800

    [Add] ProfileViewController

--- Instagram/Controllers/Core Tabs/ProfileViewController.swift
+++ Instagram/Controllers/Core Tabs/ProfileViewController.swift
@@ -7,7 +7,10 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+/// Profile view controller
+final class ProfileViewController: UIViewController {
+    
+    private var collectionView: UICollectionView? // optional because it's going to instantiate with a flow layout
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,13 +18,28 @@ class ProfileViewController: UIViewController {
         view.backgroundColor = .systemBackground
         configureNavigationBar()
         
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0) // padding between section
+        layout.itemSize = CGSize(width: view.width/3, height: view.width/3)
+        
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView?.delegate = self
+        collectionView?.dataSource = self
+        
+        guard let collectionView = collectionView else {
+            return
+        }
+        view.addSubview(collectionView)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        collectionView?.frame = view.bounds // entire screen
     }
 
     private func configureNavigationBar() {
-        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "gear"),
-                                                            style: .done,
-                                                            target: self,
-                                                            action: #selector(didTapSettingsButton))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "gear"), style: .done, target: self, action: #selector(didTapSettingsButton))
     }
     
     @objc private func didTapSettingsButton() {
@@ -29,5 +47,20 @@ class ProfileViewController: UIViewController {
         vc.title = "Settings"
         navigationController?.pushViewController(vc, animated: true)
         
+    }
+}
+
+extension ProfileViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+    
+    // When user tap on one of the cell/post
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
     }
 }
