60e1fdbf70dd231c94416ba7c33ed3596634da27
Hanna Putiprawan <hputiprawan2@gmail.com>
2020-12-29 22:23:47 -0800

    [Add] Home Feed Cells - IGFeedPostHeaderTableViewCell - IGFeedPostTableViewCell - IGFeedPostActionsTableViewCell - IGFeedPostGeneralTableViewCell
    [Notes]
    - Only add files, not yet fully implement

--- Instagram/Controllers/Core Tabs/HomeViewController.swift
+++ Instagram/Controllers/Core Tabs/HomeViewController.swift
@@ -10,9 +10,23 @@ import FirebaseAuth
 
 class HomeViewController: UIViewController {
 
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(IGFeedPostTableViewCell.self, forCellReuseIdentifier: IGFeedPostTableViewCell.identifier)
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds // set the frame to entire screen
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -31,3 +45,18 @@ class HomeViewController: UIViewController {
     }
 }
 
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: IGFeedPostTableViewCell.identifier, for: indexPath) as! IGFeedPostTableViewCell
+        
+        return cell
+    }
+}
---
+++ Instagram/Views/Cells/HomeFeedCells/IGFeedPostTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  IGFeedPostTableViewCell.swift
+//  Instagram
+//
+//  Created by Hanna Guest on 12/29/20.
+//
+
+import UIKit
+
+final class IGFeedPostTableViewCell: UITableViewCell {
+
+    static let identifier = "IGFeedPostTableViewCell"
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public func configure() {
+        // Configure the cell
+    }
+}
